03cba32e8c1ec5d7f8c297dfafe0e49aea9d24d7
Luciano Schmidt <luciano43schmidt@gmail.com>
2023-03-02 20:27:23 -0300

    (feature) added registration and login views, forms and actions, as well as a navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+import s from "./style.module.css"
+import logo from "../../assets/logo.png"
+import { logOut } from '../../redux/actions'
+
+const Navbar = ({ userInfo }) => {
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const logoutHandler = (e) => {
+        dispatch(logOut())
+        navigate("/login")
+    }
+
+    return (
+        <div className={`${s.navbar}`}>
+            <img src={logo} alt="Wiselink logo" />
+            {userInfo !== undefined ?
+                <div className={`${s.loggedUser}`}>
+                    <p className={`${s.userName}`}>{userInfo.user.firstName} {userInfo.user.lastName}</p>
+                    <button className={`${s.logout}`} onClick={logoutHandler}>Log out</button>
+                </div> : null}
+        </div>
+    )
+}
+
+export default Navbar
