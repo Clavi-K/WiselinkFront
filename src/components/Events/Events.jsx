fe630926a310a216b493924d5e63590da6d56775
Luciano Schmidt <luciano43schmidt@gmail.com>
2023-03-03 16:00:17 -0300

    (update) events container components

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -14,15 +14,42 @@ const Events = () => {
     const userInfo = useSelector(state => state.userInfo)
 
     useEffect(() => {
-        if(!userInfo) navigate("/")
+        if (!userInfo) navigate("/")
         dispatch(getEvents(userInfo.accessToken))
-    })
+    }, [])
 
     return (
+        <div>
+            <h1 className={`${s.title}`}>Available events</h1>
 
+            <div onClick={() => console.log("A")} className={`${s.eventsContainer}`}>
+                {events ?
+                    events.map(e => (
 
-        <div>Events</div>
+                        <div className={`${s.eventCard}`} key={e.id}>
+
+                            <div className={`${s.topCard}`}>
+                                <p className={`${ s.eventTitle }`}>{e.title}</p>
+                                <p className={`${ s.eventDate }`}>{dateParser(e.dateTime)}</p>
+                            </div>
+
+                            <div className={`${ s.bottomCard }`}>
+                                <p className={`${ s.eventSDescription }`}>{e.shortDescription}</p>
+                                <p>{e.status === "DRAFT" ? `(${ e.status })` : null}</p>
+                            </div>
+
+                        </div>
+                    ))
+                    : null}
+            </div>
+
+        </div>
     )
 }
 
 export default Events
+
+function dateParser(dateTime) {
+    const date = new Date(dateTime)
+    return `${ date.getDate() } / ${ date.getMonth() } / ${ date.getFullYear() }`
+}
